ae5774a8a79381d4e81f577e38734eb9efbc5d52
kazuhirosaji <kazuhirosaji@gmail.com>
2014-03-09 23:42:28 +0900

    add comments

--- geochart.js
+++ geochart.js
@@ -5,6 +5,7 @@ var gNationsData = [];
 var gTeamName = [];
 var wikiUrl = "https://ja.wikipedia.org/wiki/";
 
+/* Chart描画処理 */
 function drawRegionsMap() {
   var rankData = [];
   for (var i = 0; i < gTeamName.length; i++) {
@@ -29,6 +30,7 @@ function drawRegionsMap() {
 
   google.visualization.events.addListener(chart, 'regionClick', selectHandler);
 
+  // クリックした国のサッカー代表チームのWikiページを表示する
   function selectHandler(reg) {
     console.log(reg);
     var index = gCountryCode.indexOf(reg.region);
@@ -37,14 +39,17 @@ function drawRegionsMap() {
     console.log(countryName);
     openNationalTeamPage(countryName);
   }
-  
-  function openNationalTeamPage(name) {
-    window.open(wikiUrl + "サッカー" + name +"代表", 'jp_window', 'width=800, height=300');
-  }
 };
 
 var gCountrymaCode = [];
 
+function openNationalTeamPage(name) {
+  window.open(wikiUrl + "サッカー" + name +"代表", 'jp_window', 'width=800, height=300');
+}
+
+/* FIFAランキングで使用されている3文字の国コードを
+   GeoChartで使用するための2文字の国コードに変換する
+*/
 function changeCountryName3to2(cName, i) {
   var index = gCountrymaCode.indexOf(cName);
   console.log(cName + "=>" + gCountryCode[index]);
@@ -52,6 +57,9 @@ function changeCountryName3to2(cName, i) {
     console.log(gNationsData.nations[index].en_name);
     return gNationsData.nations[index].code;
   } else {
+    /* 対応する2文字の国コードが見つからない場合は、FIFAランキングで
+       取得した国名を使用する。
+    */
     console.log(gNationsData.ranks[i].teamName);
     return gNationsData.ranks[i].teamName;
   }
@@ -60,6 +68,10 @@ function changeCountryName3to2(cName, i) {
 function loadRanks() {
   httpObj = new XMLHttpRequest();
   httpObj.open("get", "./nations.json", true);
+  
+  /* 取得した国の地域コード(ISO-3166)、FIFAランキングデータから
+     国名[ランキング]の配列を作成する
+  */
   httpObj.onload = function(){
     console.log("http onload");
     gNationsData = JSON.parse(this.responseText);
